cc40794e0f2262af3f054243b1f3a0f9234e475f
albertkjoller <albert_k_jacobsen@hotmail.com>
2022-11-21 15:36:53 +0100

    clean up-ish for P+F

--- content/toy_example.py
+++ content/toy_example.py
@@ -6,14 +6,13 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from tqdm import trange
 
-from GNNModels import EvidentialToyModel
+from GNNModels import EvidentialToyModel1D
 from losses import NIGLoss
 
-def train(train_data, model, optimizer, lr=0.01, loss_function=torch.nn.MSELoss(), lambd=1.0,
+
+def train(dataset, model, optimizer, loss_function, lr=0.01,
           epochs=1000,  device=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'),
           store_train_every=50, val_every_step=50, tensorboard_logdir='logs', experiment_name=str(time.time())):
 
@@ -32,112 +31,58 @@ def train(train_data, model, optimizer, lr=0.01, loss_function=torch.nn.MSELoss(
     with trange(epochs) as t:
 
         for epoch in t:
-            for batch_data, batch_target in train_data:
+            for batch_data, batch_target in dataset.data: # TODO: in line with 3D Molecular graph structure
                 optimizer.zero_grad()
+
                 # Run forward pass
                 outputs = model(batch_data)
 
                 # Compute loss
-                loss, (nll_loss, reg_loss) = loss_function(outputs).compute_loss(batch_target, lambd)
+                (loss_name, loss), xtra_losses = loss_function(outputs, batch_target)
                 loss.backward()
                 optimizer.step()
 
             # Store train losses
             if epoch % store_train_every == 0:
-                writer.add_scalar('TRAIN/Loss', loss, epoch)
-                writer.add_scalar('TRAIN/NLL Loss', nll_loss, epoch)
-                writer.add_scalar('TRAIN/REG Loss', reg_loss, epoch)
-
                 train_lss[epoch] = loss
-                # TODO: add MSE for batch
-                #train_acc[epoch] = torch.mean(torch.pow(outputs[:, 0] - train_data['target'], 2))
+
+                writer.add_scalar(f'TRAIN/{loss_name}', loss, epoch)
+                if xtra_losses != None:
+                    for name, loss_ in xtra_losses:
+                        writer.add_scalar(f'TRAIN/{name}', loss_, epoch)
 
                 # Print status
                 t.set_description_str(f'Training Loss: {train_lss[epoch]:.3f} | Training MSE: {train_acc[epoch]:.3f} | Progress')
-                #print("")
 
             if epoch % val_every_step == 0:
                 model.eval()
-                # TODO: input data (black dots) are currently not used
-                plot_regression_line(model, device, plot_uncertainty=True)
+                dataset.plot_regression_line(model, device, plot_uncertainty=True)
                 model.train()
+
     # Close tensorboard
     writer.close()
 
-def plot_regression_line(model, device, plot_uncertainty=False):
-    # Predict on the data range
-    toy_ = torch.arange(-6, 6, 12 / N).reshape(-1, 1).to(device)
-    outputs = model(toy_)
-
-    # Get evidential parameters
-    gamma, v, alpha, beta = torch.tensor_split(outputs, 4, axis=1)
-
-    # Reformat tensors
-    xaxis = toy_.detach().flatten().cpu().numpy()
-    y_true = (toy_ ** 3).detach().flatten().cpu().numpy()
-    y_pred = gamma.detach().flatten().cpu().numpy()
-    aleatoric = (beta / (alpha - 1)).detach().flatten().cpu().numpy()
-    epistemic = (beta / (v * (alpha - 1))).detach().flatten().cpu().numpy()
-
-    # Gather information in dataframe for plotting
-    results = pd.DataFrame({'xaxis': xaxis,
-                            'y_true': y_true,
-                            'y_pred': y_pred,
-                            'aleatoric': aleatoric,
-                            'epistemic': epistemic})
-
-    # Plot regression line
-
-    plt.plot(results['xaxis'], results['y_true'], '--r')
-    plt.plot(results['xaxis'], results['y_pred'])
-
-    plt.vlines(-4, -6.5 ** 3, 6.5 ** 3, colors='gray', linestyles='--')
-    plt.vlines(4, -6.5 ** 3, 6.5 ** 3, colors='gray', linestyles='--')
-    plt.fill_betweenx(pd.Series(np.arange(-6.5 ** 3, 6.5 ** 3)), -6, -4, alpha=.3, interpolate=True, color='gray')
-    plt.fill_betweenx(pd.Series(np.arange(-6.5 ** 3, 6.5 ** 3)), 4, 6, alpha=.3, interpolate=True, color='gray')
-
-    if plot_uncertainty == True:
-        plt.fill_between(results['xaxis'], results['y_pred'] - results['epistemic'], results['y_pred'] + results['epistemic'], alpha=.3, interpolate=True)  # step='post')
-
-    plt.xlim([-6.5, 6.5])
-    plt.ylim([-6.5**3, 6.5**3])
-    plt.show()
 
 if __name__ == '__main__':
+
+    from Datasets import ToyDataset1D
 
     # Utilize GPU?
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    # Define data
-    N = 1024
-    B = 64
-
-    data = {}
-    order_ = torch.randperm(N) # shuffle data
-    data['data'] = torch.arange(-4, 4, 8/N)[order_].reshape(-1, B, 1).to(device)
-    data['target'] = data['data']**3 + (torch.randn(N, 1).reshape(-1, B, 1) * 3).to(device)
-
-    # plot data
-    plt.plot(data['data'].detach().flatten().cpu(), data['target'].detach().flatten().cpu(), '.')
-    plt.plot(torch.arange(-4, 4, 8/N), torch.arange(-4, 4, 8/N)**3, 'k--')
-    plt.xlim([-6.5, 6.5])
-    plt.show()
-
-    # structure data in batches
-    data = list(zip(data['data'], data['target']))
+    # Load data
+    toydata = ToyDataset1D(B=64, N=1024, device=device, visualize_on_load=True, seed=42)
 
     # Setup regression modelwork
-    model = EvidentialToyModel(hidden_dim=32, device=device)
+    model = EvidentialToyModel1D(hidden_dim=32, device=device)
     model.to(device)
 
-    loss_function = NIGLoss
-    optimizer = torch.optim.Adam(model.parameters(),)
+    # Define training parameters
+    loss_function = NIGLoss(lambd_=1.0)
+    optimizer = torch.optim.Adam(model.parameters())
 
     # Run training loop
-    train(data, model, optimizer, loss_function=loss_function, lr=0.05,
-          epochs=500, store_train_every=1, val_every_step=1, lambd=0.0, # TODO: investigate lambda
+    train(toydata, model, optimizer, loss_function=loss_function, lr=0.05,
+          epochs=500, store_train_every=1, val_every_step=1,
           experiment_name='toy_example_1D', tensorboard_logdir='../logs')
-
-
-    print("")
 
--- content/train.py
+++ content/train.py
@@ -9,10 +9,10 @@ import numpy as np
 from tqdm import trange
 
 import Datasets
-from GNNModels import GNNEvidentialInvariant
-from losses import NIGLoss
+from GNNModels import EvidentialGNN3D
+from losses import NIGLoss, MSELoss
 
-def train(train_data, test_data, net, optimizer, lr=0.01, loss_function=torch.nn.MSELoss(), epochs=1000,
+def train(train_data, test_data, net, optimizer, loss_function, lr=0.01, epochs=1000,
           val_every_step=50, store_train_every=50,
           tensorboard_logdir='logs', experiment_name=str(time.time())):
 
@@ -37,18 +37,18 @@ def train(train_data, test_data, net, optimizer, lr=0.01, loss_function=torch.nn
             outputs = net(train_data)
 
             # Compute loss
-            loss, (nll_loss, reg_loss) = loss_function(outputs).compute_loss(train_data.target, 0.1)
+            (loss_name, loss), xtra_losses = loss_function(outputs, train_data.target)
             loss.backward()
             optimizer.step()
 
             # Store train losses
             if epoch % store_train_every == 0:
-                writer.add_scalar('TRAIN/Loss', loss, epoch)
-                writer.add_scalar('TRAIN/NLL Loss', nll_loss, epoch)
-                writer.add_scalar('TRAIN/REG Loss', reg_loss, epoch)
-
                 train_lss[epoch] = loss
-                train_acc[epoch] = torch.mean(torch.pow(outputs - train_data.target, 2))
+
+                writer.add_scalar(f'TRAIN/{loss_name}', loss, epoch)
+                if xtra_losses != None:
+                    for name, loss_ in xtra_losses:
+                        writer.add_scalar(f'TRAIN/{name}', loss_, epoch)
 
             # Validation run
             if epoch % val_every_step == 0:
@@ -56,15 +56,16 @@ def train(train_data, test_data, net, optimizer, lr=0.01, loss_function=torch.nn
                     # Change network mode
                     net.eval()
                     outputs = net(test_data)
-                    loss, (nll_loss, reg_loss) = loss_function(outputs).compute_loss(test_data.target, 0.1)
 
-                    # Store validation losses
-                    writer.add_scalar('VALIDATION/Loss', loss, epoch)
-                    writer.add_scalar('VALIDATION/NLL Loss', nll_loss, epoch)
-                    writer.add_scalar('VALIDATION/REG Loss', reg_loss, epoch)
+                    # Compute loss
+                    (loss_name, loss), xtra_losses = loss_function(outputs, test_data.target)
 
                     val_lss[val_step] = loss
-                    val_acc[val_step] = torch.mean(torch.pow(outputs - test_data.target, 2))
+                    writer.add_scalar(f'VALIDATION/{loss_name}', loss, epoch)
+                    if xtra_losses != None:
+                        for name, loss_ in xtra_losses:
+                            writer.add_scalar(f'VALIDATION/{name}', loss_, epoch)
+
                     net.train()
 
                 # Print status
@@ -87,9 +88,8 @@ if __name__ == '__main__':
     test_data = QM9.data['test']
 
     # Setup regression network
-    net = GNNEvidentialInvariant(state_dim=10, num_message_passing_rounds=3, device=device)
-    #loss_function = torch.nn.MSELoss()
-    loss_function = NIGLoss
+    net = EvidentialGNN3D(state_dim=10, num_message_passing_rounds=3, device=device)
+    loss_function = MSELoss()
     optimizer = torch.optim.Adam(net.parameters(),)
 
     # Training parameters
@@ -97,7 +97,7 @@ if __name__ == '__main__':
 
     # Tensorboard configurations
     tensorboard_logdir = '../logs/'
-    experiment_name = 'evidential_test'
+    experiment_name = 'P_F_test'
 
     # Run training loop
     train(train_data, test_data, net, optimizer, loss_function=loss_function, lr=0.01,
